9ce7828e2f282f48f390dfc6489b40c7e984ef58
sharull <sheikh.arafat.ulla@gmail.com>
2026-07-05 20:54:51 +0530

    feat: integrate @workspace/eslint-config and update ESLint configurations for improved consistency

--- packages/eslint-config/react-internal.js
+++ packages/eslint-config/react-internal.js
@@ -1,9 +1,6 @@
-import js from "@eslint/js"
-import eslintConfigPrettier from "eslint-config-prettier"
 import pluginReact from "eslint-plugin-react"
 import pluginReactHooks from "eslint-plugin-react-hooks"
 import globals from "globals"
-import tseslint from "typescript-eslint"
 
 import { config as baseConfig } from "./base.js"
 
@@ -13,16 +10,13 @@ import { config as baseConfig } from "./base.js"
  * @type {import("eslint").Linter.Config} */
 export const config = [
   ...baseConfig,
-  js.configs.recommended,
-  eslintConfigPrettier,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
   {
+    ...pluginReact.configs.flat.recommended,
     languageOptions: {
       ...pluginReact.configs.flat.recommended.languageOptions,
       globals: {
-        ...globals.serviceworker,
         ...globals.browser,
+        ...globals.serviceworker,
       },
     },
   },
@@ -30,10 +24,13 @@ export const config = [
     plugins: {
       "react-hooks": pluginReactHooks,
     },
-    settings: { react: { version: "detect" } },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
     rules: {
       ...pluginReactHooks.configs.recommended.rules,
-      // React scope no longer necessary with new JSX transform.
       "react/react-in-jsx-scope": "off",
       "react/prop-types": "off",
     },
